7c9927383db26c8c3885a58e323a60dc1c347cd2
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2023-02-14 09:43:14 -0800

    (DEPLOY) Add Syntax Highlighting, Social Anchors, Error Pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,38 @@
-require 'github/markup'
-require 'uri'
+#require 'github/markup'
+#require 'uri'
+
+#require 'commonmarker'
 
 class ArticlesController < ApplicationController
-  
+  class HTMLwithPygments < Redcarpet::Render::HTML
+      def block_code(code, language)
+          Pygments.highlight(code, lexer: language)
+      end
+  end
+
+  def markdown(text)
+      renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+
+      options = {
+          autolink: true,
+          no_intra_emphasis: true,
+          fenced_code_blocks: true,
+          lax_html_blocks: true,
+          strikethrough: true,
+          superscript: true,
+      }
+      
+      Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
+
   def index
     @parent_path = request.path
     puts "PATH = " + @parent_path
   end
 
   def open_article
-
     title = Rails.root.to_s + "/app/markdown/" + params[:title] + ".md"
-    @content = ActionController::Base.render inline: GitHub::Markup.render(title, File.read(title))
+    @content = markdown(File.read(title))
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
 module ApplicationHelper
-    def current?(key, path)
-        "#{key}" if current_page?('/articles/' + path)
-     end
+    
 end
